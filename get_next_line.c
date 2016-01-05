07b63f8ec0a3cd2e03c25e31328e98508c71ea43
Quentin ROULON <quroulon@e3r9p16.42.fr>
2016-01-05 18:26:52 +0100

    nouvelle facon de free

--- get_next_line.c
+++ get_next_line.c
@@ -12,14 +12,15 @@
 
 #include "get_next_line.h"
 
-// static void		ft_freestr(char *str)
-// {
-// 	str--;
-// 	while (*str)
-// 		str--;
-// 	free(str);
-// 	str = NULL;
-// }
+static void		ft_freestr(char **str, int *j)
+{
+	*j = 0;
+	// (*str)--;
+	// while (**str)
+	// 	(*str)--;
+	// free(*str);
+	*str = NULL;
+}
 
 int				ft_lstcontentsize(t_list *begin)
 {
@@ -41,55 +42,39 @@ static int		ft_strinit(int fd, char **str)
 	char		*buf;
 	int			i;
 	t_list		*begin;
+	int			last;
 
 	buf = (char*)malloc(sizeof(char) * BUFF_SIZE);
-	i = 15;
 	begin = NULL;
+	last = 0;
 	while ((i = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		ft_putstr(buf);
-		ft_lstpush(&begin, buf, BUFF_SIZE);
-		ft_bzero(buf, BUFF_SIZE);
+		last = i;
+		ft_lstpush(&begin, buf, i);
+		ft_bzero(buf, i);
 	}
-	(*str) = (char*)malloc(sizeof(char) * ft_lstcontentsize(begin) + 2);
+	i = (i == -1) ? -1 : last;
+	(*str) = (char*)malloc(sizeof(char) * ft_lstcontentsize(begin) + 3);
 	(*str)[0] = '\0';
 	(*str)++;
 	ft_lsttochar(begin, (*str));
-	// ft_putnbrdl(ft_lstcontentsize(begin) + 1);
-	// (*str)[ft_lstcontentsize(begin) + 1] = '\0';
-	// ft_putstr((*str));
+	(*str)[ft_lstcontentsize(begin) + 1] = '\0';
+	(*str)[ft_lstcontentsize(begin) + 2] = '0';
 	free(begin);
 	free(buf);
 	return (i);
 }
 
-// static int		ft_findln(char *str)
-// {
-// 	int			i;
-// 	int			nb_ln;
-
-// 	i = 0;
-// 	nb_ln = 0;
-// 	while (str[i] != '\0')
-// 	{
-// 		if (str[i] == '\n')
-// 			nb_ln++;
-// 		else if (str[i] != '\n' || str[i] != '\0')
-// 			return (0);
-// 		i++;
-// 	}
-// 	return (nb_ln);
-// }
-
 int				get_next_line(int const fd, char **line)
 {
 	static char		*str = NULL;
+	static int		j = 0;
 	int				i;
 
 	i = 0;
 	*line = NULL;
 	if (str == NULL)
-		if (ft_strinit(fd, &str) < 0)
+		if ((j = ft_strinit(fd, &str)) < 0)
 			return (-1);
 	while (str[i] != '\0' && str[i] != '\n')
 		i++;
@@ -104,17 +89,10 @@ int				get_next_line(int const fd, char **line)
 	(*line)[i] = '\0';
 	if (*str != '\0')
 		str++;
-	if (*str == '\0' && ft_strcmp(*line, str) == 0)
+	if (*str == '\0' && j != 0 && ft_strcmp(*line, str) == 0)
+	{
+		ft_freestr(&str, &j);
 		return (0);
-	// else if (str[0] == '\0' && str[1] == '0')
-	// {
-	// 	return (0);
-	// }
-	// else if (str[0] == '\0')
-	// 	str[1] = '0';
-	// else if (*str == '\0' && --(*str) == '\n')
-	// 	return (0);
-	// if (*str == '\0' && j > 0)
-	// 	ft_freestr(str);
+	}
 	return (1);
 }
--- main.c
+++ main.c
@@ -17,45 +17,93 @@
 ** go='gccw -I libft/includes -c get_next_line.c main.c; gcc -o gnt get_next_line.o main.o -L libft -lft;'
 */
 
+// int			main(void)
+// {
+// 	int				fd;
+// 	char			*line;
+// 	int				p[2];
+// 	int				save_fd;
+
+// 	fd = 1;
+// 	save_fd = dup(fd);
+// 	pipe(p);
+
+// 	dup2(p[1], fd);
+// 	write(fd, "aaa", 3);
+// 	close(p[1]);
+// 	dup2(save_fd, fd);
+// 	get_next_line(p[0], &line);
+// 	ft_putstr(line);
+// 	free(line);
+// 	return (0);
+// }
+
+// int			main(int ac, char **av)
+// {
+// 	int				fd;
+// 	char			*line;
+
+// 	fd = open(av[1], O_RDONLY);
+// 	line = NULL;
+// 	if (ac == 2 && fd != -1)
+// 	{
+// 		while (get_next_line(fd, &line) > 0)
+// 		{
+// 			ft_putstr(line);
+// 		}
+// 	}
+// 	close(fd);
+// 	free(line);
+// 	return (0);
+// }
+
 int			main(int ac, char **av)
 {
 	int				fd;
 	char			*line;
 
 	fd = open(av[1], O_RDONLY);
+	// fd = -45;
 	line = NULL;
 	if (ac == 2 && fd != -1)
-	{
-		// get_next_line(fd, &line);
-		while (get_next_line(fd, &line) > 0)
-		{
-			// ft_putstr(line);
-		}
-	}
-	// else
-	// 	ft_putendl("error");
-	// ft_putstr(line);
-	// get_next_line(fd, &line);
-	// ft_putstr(line);
-	// get_next_line(fd, &line);
-	// ft_putstr(line);
-	// get_next_line(fd, &line);
-	// ft_putstr(line);
-	// get_next_line(fd, &line);
-	// ft_putstr(line);
-	// get_next_line(fd, &line);
-	// ft_putstr(line);
-	// get_next_line(fd, &line);
-	// ft_putstr(line);
-	// get_next_line(fd, &line);
-	// ft_putstr(line);
-	// get_next_line(fd, &line);
-	// ft_putstr(line);
-	// get_next_line(fd, &line);
-	// ft_putstr(line);
-	// get_next_line(fd, &line);
-	// ft_putstr(line);
+		ft_putnbrdl(get_next_line(fd, &line));
+	else
+		ft_putendl("error");
+	ft_putstr(line);
+	get_next_line(fd, &line);
+	ft_putstr(line);
+	get_next_line(fd, &line);
+	ft_putstr(line);
+	get_next_line(fd, &line);
+	ft_putstr(line);
+	get_next_line(fd, &line);
+	ft_putstr(line);
+	get_next_line(fd, &line);
+	ft_putstr(line);
+	get_next_line(fd, &line);
+	ft_putstr(line);
+	get_next_line(fd, &line);
+	ft_putstr(line);
+	get_next_line(fd, &line);
+	ft_putstr(line);
+	get_next_line(fd, &line);
+	ft_putstr(line);
+	get_next_line(fd, &line);
+	ft_putstr(line);
 	close(fd);
 	free(line);
 	return (0);
 }
+
+
+	// fd = 1;
+	// out = dup(fd); // créé une copie du fd fd dans out avec le plus pti nombre pas use
+	// pipe(p); // Génère un tube, 0 lecture, 1 écriture. 1 est gardé en mémoire tant qu'il n'est pas lu
+
+	// dup2(p[1], fd); // Créé une copie du fd p[1] dans le fd fd
+	// write(fd, "aaa", 3); // Met aaa dans le fd p[1].
+	// close(p[1]); // ferme p[1]
+	// dup2(out, fd); // recré fd avec out (qui est la position de départ de fd)
+	// gnl_ret = get_next_line(p[0], &line); // Lit le fd p[0] qui contient aaa
+	// mt_assert(strcmp(line, "aaa") == 0);
+	// mt_assert(gnl_ret == 0 || gnl_ret == 1);
