ae7707db11e6bbefb7a33bca81aa1aa8eeaca951
Quentin ROULON <quroulon@e2r3p19.42.fr>
2016-01-13 19:57:15 +0100

    modifs

--- get_next_line.c
+++ get_next_line.c
@@ -12,41 +12,32 @@
 
 #include "get_next_line.h"
 
-static int			ft_read(t_list **str, int fd, t_list *tmp)
+static int			ft_read(t_list **str, int fd, t_list *tmp, int i)
 {
-	int				i;
 	char			*buf;
 	t_list			*list;
-	t_list			*list_tmp;
 
-	i = 0;
 	buf = ft_strnew(BUFF_SIZE);
-	list_tmp = NULL;
+	list = NULL;
 	while ((i = read(fd, buf, BUFF_SIZE)) > 0)
 		ft_lstpush(&list, buf, i);
 	ft_strdel(&buf);
 	if (i == -1)
 		return (-1);
-	list_tmp->content = (char*)malloc(sizeof(char) * ft_lstcontentsize(list) + 2);
-	((char*)list_tmp->content)[0] = '\2';
-	list_tmp->content++;
-	list_tmp->content = ft_lsttochar(list, buf);
-	list_tmp->content_size = fd;
-	if (tmp == NULL)
-		*str = list_tmp;
-	else
-		tmp->next = list_tmp;
+	buf = NULL;
+	buf = (char*)malloc(sizeof(char) * ft_lstcontentsize(list) + 2);
+	ft_bzero(buf, ft_lstcontentsize(list) + 2);
+	buf[0] = '\2';
+	buf++;
+	ft_lsttochar(list, buf);
+	buf--;
+	ft_lstpush(str, buf, ft_lstcontentsize(list) + 2);
+	tmp = *str;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->content_size = fd;
 	ft_lstclear(&list);
-	// list = NULL;
-	// list->content
-
-
-
-	// ft_lstpush(str, buf, ft_lstcontentsize(list) + 1);
-	// if (tmp != NULL)
-	// 	tmp = tmp->next;
-	// tmp->content_size = (tmp->content_size * 1000) + fd;
-	// ft_strdel(&buf);
+	ft_strdel(&buf);
 	return (i);
 }
 
@@ -58,18 +49,11 @@ int					get_next_line(int const fd, char **line)
 	tmp = str;
 	if (line == NULL)
 		return (-1);
-	if (str == NULL)
-	{
-		if (ft_read(&str, fd, tmp) < 0)
+	while (tmp && tmp->content_size != (size_t)fd)
+		tmp = tmp->next;
+	if (tmp == NULL)
+		if (ft_read(&str, fd, tmp, 0) < 0)
 			return (-1);
-	}
-	else
-	{	
-		while (tmp->next && tmp->content_size != (size_t)fd)
-			tmp = tmp->next;
-		if (tmp->content_size != (size_t)fd)
-			if (ft_read(&str, fd, tmp) < 0)
-				return (-1);
-	}
-	return (1);
+	ft_read();
+	return (0);
 }
